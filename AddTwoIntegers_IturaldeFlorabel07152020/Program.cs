2787da7c9892e6cfe6b471949ebda24f93aafed8
Florabel Ituralde <67927764+florabelituralde@users.noreply.github.com>
2020-08-05 10:24:32 -0400

    Add files via upload

---
+++ AddTwoIntegers_IturaldeFlorabel07152020/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoIntegers_IturaldeFlorabel07152020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+            Console.WriteLine("Calculate the sum of two numbers: ");
+            Console.Write("Input number 1: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input number 2: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            sum = num1 + num2;
+            Console.Write("Result:" + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
